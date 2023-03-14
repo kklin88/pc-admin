540c4a3617458dde1ed5850fc1d7e84d39175642
kklin88 <linkui21@gmail.com>
2023-03-14 20:09:49 +0800

    v0.1

---
+++ pc-admin/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <MenuList class="menu"></MenuList>
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import MenuList from "./components/MenuList/index.vue";
+export default {
+  name: "App",
+  components: {
+    MenuList,
+  },
+};
+</script>
+<style lang="less">
+@menuWidth: 300px;
+#app {
+  .menu {
+    position: fixed;
+    width: @menuWidth;
+    top: 0;
+    bottom: 0;
+    left: 0;
+  }
+  .content {
+    margin-left: @menuWidth;
+  }
+}
+body {
+  margin: 0;
+  background: #f5f5fb;
+}
+</style>
---
+++ pc-admin/src/pages/HomePage/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <el-image
+      class="img"
+      v-for="url in urls"
+      :key="url"
+      :src="url"
+      lazy
+    ></el-image>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: {},
+  data() {
+    return {
+      urls: [],
+    };
+  },
+  async created() {
+    const res = await fetch("/api/getPicList");
+    const resJson = await res.json();
+    this.urls = resJson.data;
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.container {
+	.img {
+		width: 100%;
+	}
+}
+</style>
