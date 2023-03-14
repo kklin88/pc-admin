c3a745614a447357e9a921dae8a72574af28ff59
kklin88 <linkui21@gmail.com>
2023-03-14 21:08:21 +0800

    web

--- pc-admin/deploy.sh
+++ pc-admin/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:kklin88/pc-admin-website.git master:gh-pages
+git push -f https://github.com/kklin88/pc-admin.git main:gh-pages
 
 cd -
